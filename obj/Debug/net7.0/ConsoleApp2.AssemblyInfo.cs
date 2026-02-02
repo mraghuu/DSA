8a3091200a95a449c81678f2e2044315745418bb
Raghu <raghuknps@gmail.com>
2026-02-02 23:08:52 +0530

    commited new changes

--- obj/Debug/net7.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net7.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c4706eaa0cd73f0cb17b0818bd33c809a201d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
